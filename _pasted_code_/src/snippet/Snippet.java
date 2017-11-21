15072d1a2ae7be1826f91d6a599b1824dd8f5f9d
Teemu <nospam@gmail.com>
2017-11-21 08:19:15 +0530

    commited selenium Basic Data

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,16 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello");
+		
+		System.out.println("hii");
+		
+		System.out.println("this is rahul patidar");
+		
+		
+	}
+}
+
